ef90d2e30fa5484002058a9efbec1ee563841b4e
powerfulh <hanhoon12@naver.com>
2024-10-18 19:42:42 +0900

    Init

---
+++ src/test/java/toy/powerfulh/spring_secu/SpringSecuApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.powerfulh.spring_secu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
