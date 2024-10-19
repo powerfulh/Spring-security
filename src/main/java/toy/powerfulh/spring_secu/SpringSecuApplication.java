0068ad24265377c56073a73754b7d9f759adb623
powerfulh <hanhoon12@naver.com>
2024-10-19 13:28:25 +0900

    설정 파일 형식 YML

--- src/main/java/toy/powerfulh/spring_secu/SpringSecuApplication.java
+++ src/main/java/toy/powerfulh/spring_secu/SpringSecuApplication.java
@@ -2,12 +2,15 @@ package toy.powerfulh.spring_secu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringSecuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecuApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(SpringSecuApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run();
 	}
 
 }
